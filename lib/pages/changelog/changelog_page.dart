2505e4a8b89a8ced47e2e0ea02816886bdc8a808
SiongSng <rrt467778@gmail.com>
2022-01-25 16:04:38 +0800

    編輯紀錄頁面優化

--- lib/pages/changelog/changelog_page.dart
+++ lib/pages/changelog/changelog_page.dart
@@ -1,5 +1,5 @@
+import 'package:diff_match_patch/diff_match_patch.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:rpmtw_api_client_flutter/rpmtw_api_client_flutter.dart';
 import 'package:rpmtw_wiki/pages/base_page.dart';
 import 'package:rpmtw_wiki/utilities/data.dart';
@@ -19,8 +19,8 @@ class ChangelogPage extends StatefulWidget {
 class _ChangelogPageState extends State<ChangelogPage> {
   RPMTWApiClient apiClient = RPMTWApiClient.lastInstance;
 
-  Future<List<WikiChangeLog>> load() async {
-    List<WikiChangeLog> changelogs =
+  Future<List<WikiChangelog>> load() async {
+    List<WikiChangelog> changelogs =
         await apiClient.minecraftResource.filterChangelogs();
     return changelogs;
   }
@@ -29,15 +29,15 @@ class _ChangelogPageState extends State<ChangelogPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TitleBar(
-        title: "編輯紀錄",
+        title: localizations.changelogTitle,
         onBackPressed: () => navigation.pop(),
       ),
       body: BasePage(
-          child: FutureBuilder<List<WikiChangeLog>>(
+          child: FutureBuilder<List<WikiChangelog>>(
         future: load(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<WikiChangeLog> changelogs = snapshot.data!;
+            List<WikiChangelog> changelogs = snapshot.data!;
             return SizedBox(
               height: Utility.getSize(context).height * 0.8,
               width: Utility.getSize(context).width,
@@ -48,32 +48,106 @@ class _ChangelogPageState extends State<ChangelogPage> {
                     child: ListView.builder(
                         itemCount: changelogs.length,
                         itemBuilder: (context, index) {
-                          WikiChangeLog changelog = changelogs[index];
+                          WikiChangelog changelog = changelogs[index];
 
                           return FutureBuilder<User>(
                               future: changelog.user,
                               builder: (context, snapshot) {
                                 if (snapshot.hasData) {
                                   User user = snapshot.data!;
-                                  WikiChangeLogType type = changelog.type;
+                                  WikiChangelogType type = changelog.type;
 
                                   Widget _buildChangelogType() {
-                                    if (type == WikiChangeLogType.addedMod) {
-                                      return const Tooltip(
-                                          child: Icon(Icons.add),
-                                          message: "建立模組");
+                                    if (type == WikiChangelogType.addedMod) {
+                                      return Tooltip(
+                                          child: const Icon(Icons.add),
+                                          message: localizations
+                                              .changelogTypeAddedMod);
                                     } else if (type ==
-                                        WikiChangeLogType.editedMod) {
-                                      return const Tooltip(
-                                          child: Icon(Icons.edit),
-                                          message: "編輯模組");
+                                        WikiChangelogType.editedMod) {
+                                      return Tooltip(
+                                          child: const Icon(Icons.edit),
+                                          message: localizations
+                                              .changelogTypeEditedMod);
                                     } else if (type ==
-                                        WikiChangeLogType.removedMod) {
-                                      return const Tooltip(
-                                          child: Icon(Icons.delete),
-                                          message: "移除模組");
+                                        WikiChangelogType.removedMod) {
+                                      return Tooltip(
+                                          child: const Icon(Icons.delete),
+                                          message: localizations
+                                              .changelogTypeRemovedMod);
                                     } else {
                                       return const Icon(Icons.error);
+                                    }
+                                  }
+
+                                  Widget _buildDataView() {
+                                    if (type == WikiChangelogType.addedMod ||
+                                        type == WikiChangelogType.editedMod ||
+                                        type == WikiChangelogType.removedMod) {
+                                      MinecraftMod mod = MinecraftMod.fromMap(
+                                          changelog.changedData);
+                                      String name = mod.name;
+
+                                      if (mod.translatedName != null) {
+                                        name += " (${mod.translatedName!})";
+                                      }
+                                      if (type == WikiChangelogType.editedMod) {
+                                        return SEOSelectableText(localizations
+                                            .changelogEditedData(name));
+                                      } else if (type ==
+                                          WikiChangelogType.addedMod) {
+                                        return SEOSelectableText(localizations
+                                            .changelogCreatedData(name));
+                                      } else {
+                                        return SEOSelectableText(localizations
+                                            .changelogRemovedData(name));
+                                      }
+                                    } else {
+                                      return const SizedBox.shrink();
+                                    }
+                                  }
+
+                                  Widget _buildDataCompare() {
+                                    if (type == WikiChangelogType.addedMod ||
+                                        type == WikiChangelogType.editedMod ||
+                                        type == WikiChangelogType.removedMod) {
+                                      String data1 =
+                                          changelog.changedData.toString();
+
+                                      String? data2 =
+                                          changelog.unchangedData?.toString();
+
+                                      if (data2 == null) {
+                                        return SEOSelectableText(
+                                            "+${data1.length}");
+                                      }
+
+                                      DiffMatchPatch dmp = DiffMatchPatch();
+                                      List<Diff> diffs = dmp.diff(data1, data2);
+                                      int result = 0;
+
+                                      for (Diff diff in diffs) {
+                                        if (diff.operation == DIFF_INSERT) {
+                                          result += diff.text.length;
+                                        } else if (diff.operation ==
+                                            DIFF_DELETE) {
+                                          result -= diff.text.length;
+                                        }
+                                      }
+
+                                      String str;
+
+                                      if (result == 0) {
+                                        str = localizations.changelogNone;
+                                      } else if (result > 0) {
+                                        str = "+$result";
+                                      } else {
+                                        str = "-$result";
+                                      }
+
+                                      return SEOSelectableText(str);
+                                    } else {
+                                      return const SizedBox.shrink();
                                     }
                                   }
 
@@ -85,14 +159,33 @@ class _ChangelogPageState extends State<ChangelogPage> {
 
                                   return ListTile(
                                     leading: _buildUserImage(),
-                                    title: SEOSelectableText(
-                                        changelog.changelog ?? ""),
+                                    title: SizedBox(
+                                      height: 25,
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          SEOSelectableText(
+                                              changelog.changelog ?? ""),
+                                          const RPMTWVerticalDivider(),
+                                          _buildDataView(),
+                                          const RPMTWVerticalDivider(),
+                                          _buildDataCompare()
+                                        ],
+                                      ),
+                                    ),
                                     subtitle: SEOSelectableText(
                                         Utility.dateFormat(changelog.time)),
                                     trailing: _buildChangelogType(),
                                   );
                                 } else {
-                                  return const CircularProgressIndicator();
+                                  return Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      SizedBox(height: kSplitHight),
+                                      const CircularProgressIndicator(),
+                                      SizedBox(height: kSplitHight),
+                                    ],
+                                  );
                                 }
                               });
                         }),
